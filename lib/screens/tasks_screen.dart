968ad572c4a8237b07d27a315c82ad3fc3bac251
tatsuyaohyama <t.ohyama0808@gmail.com>
2020-12-28 13:33:27 +0900

    エンプティステートを追加する

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -4,6 +4,7 @@ import 'package:todo_app/providers/task.dart';
 import 'package:todo_app/providers/tasks.dart';
 import 'package:todo_app/screens/add_task_screen.dart';
 import 'package:todo_app/screens/edit_task_screen.dart';
+import 'package:todo_app/widgets/task_empty.dart';
 
 class TasksScreen extends StatefulWidget {
   static const routeName = '/tasks';
@@ -65,99 +66,101 @@ class _TasksScreenState extends State<TasksScreen> {
           ? Center(
               child: CircularProgressIndicator(),
             )
-          : CustomScrollView(
-              shrinkWrap: true,
-              slivers: [
-                SliverList(
-                  delegate: SliverChildListDelegate(
-                    [
-                      Container(
-                        padding: EdgeInsets.only(
-                          top: 80.0,
-                          left: 60.0,
-                          right: 60.0,
-                        ),
-                        child: Text(
-                          'My Tasks',
-                          style: TextStyle(
-                            fontSize: 32.0,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ),
-                    ],
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(
+                    top: 80.0,
+                    left: 60.0,
+                    right: 60.0,
+                  ),
+                  child: Text(
+                    'My Tasks',
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                      return ChangeNotifierProvider.value(
-                        value: tasks[index],
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 20.0,
-                            vertical: 5.0,
-                          ),
-                          child: GestureDetector(
-                            behavior: HitTestBehavior.opaque,
-                            onTap: () async {
-                              final result = await Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => EditTaskScreen(
-                                    task: tasks[index],
-                                  ),
-                                ),
-                              );
-                              if (result != null)
-                                try {
-                                  await Provider.of<Tasks>(context,
-                                          listen: false)
-                                      .deleteTask(result)
-                                      .then((_) {
-                                    Scaffold.of(context).showSnackBar(
-                                      SnackBar(
-                                        content: Text(
-                                          '１件のタスクを削除しました',
+                tasks.isEmpty
+                    ? TaskEmpty()
+                    : CustomScrollView(
+                        shrinkWrap: true,
+                        slivers: [
+                          SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (BuildContext context, int index) {
+                                return ChangeNotifierProvider.value(
+                                  value: tasks[index],
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 20.0,
+                                      vertical: 5.0,
+                                    ),
+                                    child: GestureDetector(
+                                      behavior: HitTestBehavior.opaque,
+                                      onTap: () async {
+                                        final result = await Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                EditTaskScreen(
+                                              task: tasks[index],
+                                            ),
+                                          ),
+                                        );
+                                        if (result != null)
+                                          try {
+                                            await Provider.of<Tasks>(context,
+                                                    listen: false)
+                                                .deleteTask(result)
+                                                .then((_) {
+                                              Scaffold.of(context).showSnackBar(
+                                                SnackBar(
+                                                  content: Text(
+                                                    '１件のタスクを削除しました',
+                                                  ),
+                                                ),
+                                              );
+                                            });
+                                          } catch (error) {
+                                            print('error');
+                                          }
+                                      },
+                                      child: Consumer<Task>(
+                                        builder: (context, task, _) => Row(
+                                          children: [
+                                            Checkbox(
+                                              activeColor: Colors.blueAccent,
+                                              value: task.isDone,
+                                              onChanged: (value) =>
+                                                  task.toggleDoneStatus(),
+                                            ),
+                                            SizedBox(
+                                              width: 15.0,
+                                            ),
+                                            Text(
+                                              task.title,
+                                              style: TextStyle(
+                                                fontSize: 16.0,
+                                                decoration: task.isDone
+                                                    ? TextDecoration.lineThrough
+                                                    : TextDecoration.none,
+                                              ),
+                                            ),
+                                          ],
                                         ),
                                       ),
-                                    );
-                                  });
-                                } catch (error) {
-                                  print('error');
-                                }
-                            },
-                            child: Consumer<Task>(
-                              builder: (context, task, _) => Row(
-                                children: [
-                                  Checkbox(
-                                    activeColor: Colors.blueAccent,
-                                    value: task.isDone,
-                                    onChanged: (value) =>
-                                        task.toggleDoneStatus(),
-                                  ),
-                                  SizedBox(
-                                    width: 15.0,
-                                  ),
-                                  Text(
-                                    task.title,
-                                    style: TextStyle(
-                                      fontSize: 16.0,
-                                      decoration: task.isDone
-                                          ? TextDecoration.lineThrough
-                                          : TextDecoration.none,
                                     ),
                                   ),
-                                ],
-                              ),
+                                );
+                              },
+                              childCount: tasks.length,
                             ),
                           ),
-                        ),
-                      );
-                    },
-                    childCount: tasks.length,
-                  ),
-                ),
+                        ],
+                      ),
               ],
             ),
     );
